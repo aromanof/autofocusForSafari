21c429efbe4083750b55ccbc8fd496a46bbc653b
a.romanov <a.romanov@concert.ua>
2018-11-29 17:29:53 +0200

    second

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let button = document.getElementById('button');
 let input = document.getElementById('input');
 
-function action() {
-    console.log('clicked');
+// function action() {
+//     console.log('clicked');
+//     input.style.display = 'block';
+//     input.focus();
+// }
+
+button.addEventListener('click', e => {
     input.style.display = 'block';
     input.focus();
-}
+})
